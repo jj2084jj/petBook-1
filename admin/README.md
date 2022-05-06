6acd02c0f861ab5a6b8fe249afdef537dab04c0b
tails5555 <tails5555@naver.com>
2022-05-06 23:18:24 +0900

    [Kang] Project Init.

---
+++ admin/README.md
@@ -0,0 +1,21 @@
+# PetBook Back-End Admin Side
+
+## Subjects
+
+- 이 모듈 내부에선 가시성을 판단하기 위한 데이터들을 관리해야 한다.
+
+## Missions
+
+- 데이터 CRUD 를 위한 Grid Table 형성 (REST API 로 대체해도 괜찮다.)
+
+## Modeling
+
+데이터 모델링은 아래 사이트에서 관리 중이며, 일부 개선될 수도 있음을 알립니다.
+
+* [ER Diagram Logical Modeling](https://miro.com/app/board/uXjVO3-jqbI=/)
+* ER Diagram Physical Modeling: TODO
+
+## Authors
+
+* 강인성 (tails5555)
+* 한승연 (mjk6828)
---
+++ server/src/main/kotlin/net/petbook/server/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package net.petbook.server
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(ServerApplication::class.java)
+    }
+
+}
